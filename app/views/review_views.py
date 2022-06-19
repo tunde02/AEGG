6931e54419ba0236b1c5e7c87c96dcad44ae9c0d
SeongHo Jin <snskrktja@naver.com>
2022-06-20 05:08:23 +0900

    리뷰 수정 기능 추가

--- app/views/review_views.py
+++ app/views/review_views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, url_for, g
+from flask import Blueprint, flash, render_template, request, url_for, g
 from werkzeug.utils import redirect
+from sqlalchemy.sql import func
 
 from app import db
 from app.forms import ReviewForm
@@ -32,14 +33,14 @@ def create_card_review(card_id):
             create_date=datetime.now()
         )
 
-        card.avg_score = (card.avg_score * (len(card.review_list) - 1) + review.score) / len(card.review_list)
+        card.avg_score = float(db.session.query(func.avg(CardReview.score).label('avg_score')).filter(CardReview.card_id == card.id).first().avg_score)
 
         db.session.add(review)
         db.session.commit()
 
         return redirect(url_for('wiki.card_detail', card_id=card.id))
 
-    return render_template('review/review_form.html', form=form, card=card)
+    return render_template('review/review_form.html', form=form, wiki_info=card)
 
 
 @bp.route('/delete/card/<int:review_id>', methods=['GET', 'POST'])
@@ -54,7 +55,25 @@ def delete_card_review(review_id):
 @bp.route('/modify/card/<int:review_id>', methods=['GET', 'POST'])
 @login_required
 def modify_card_review(review_id):
-    # TODO
-    print('Modify Card Review')
+    card_review = CardReview.query.get_or_404(review_id)
 
-    return redirect(url_for('wiki.card_list'))
+    if g.user != card_review.user:
+        flash('수정 권한이 없습니다.')
+        return redirect(url_for('wiki.card_detail', card_id=card_review.card_id))
+
+    if request.method == 'POST':
+        form = ReviewForm()
+
+        if form.validate_on_submit():
+            form.populate_obj(card_review)
+            card_review.modify_date = datetime.now()
+
+            card_review.card.avg_score = float(db.session.query(func.avg(CardReview.score).label('avg_score')).filter(CardReview.card_id == card_review.card.id).first().avg_score)
+
+            db.session.commit()
+
+            return redirect(url_for('wiki.card_detail', card_id=card_review.card_id))
+    else:
+        form = ReviewForm(obj=card_review)
+
+    return render_template('review/review_form.html', form=form, wiki_info=card_review.card)
