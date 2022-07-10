661f2b1d886139d127a8cdc188804133d445ea9e
SeongHo Jin <snskrktja@naver.com>
2022-07-11 04:32:59 +0900

    메인 페이지 추가

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, g
 
+from app.models import Mage, MageEN, Card, CardEN, Nemesis, NemesisEN, Post
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -11,4 +12,11 @@ def load_navbar_tab():
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    mage_list = Mage.query.join(MageEN).order_by(Mage.avg_score.desc(), MageEN.name.asc()).paginate(1, per_page=5)
+    card_list = Card.query.join(CardEN).order_by(Card.avg_score.desc(), CardEN.name.asc()).paginate(1, per_page=5)
+    nemesis_list = Nemesis.query.join(NemesisEN).order_by(Nemesis.avg_score.desc(), NemesisEN.name.asc()).paginate(1, per_page=5)
+    post_list = Post.query.order_by(Post.create_date.desc()).paginate(1, per_page=15)
+
+    return render_template('index.html',
+                           mage_list=mage_list, card_list=card_list, nemesis_list=nemesis_list,
+                           post_list=post_list)
