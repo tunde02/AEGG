5e1c4f011ff85bb0a9bdddd55aa7aa10be6e3103
SeongHo Jin <snskrktja@naver.com>
2022-09-12 04:25:03 +0900

    업로드 가능한 이미지 파일 형식 추가

--- config/default.py
+++ config/default.py
@@ -3,5 +3,5 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+ALLOWED_EXTENSIONS = set(["tif", "pjp", "xbm", "svgz", "jpg", "jpeg", "ico", "tiff", "gif", "svg", "jfif", "webp", "png", "bmp", "pjpeg", "avif"])
 UPLOAD_FOLDER = './app/static/images'
