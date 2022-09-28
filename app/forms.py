b7d67588948fa3c13ced41ed85cad2384dcc1ccb
SeongHo Jin <snskrktja@naver.com>
2022-09-29 04:21:31 +0900

    허용되는 이미지 파일 확장자 추가

--- app/forms.py
+++ app/forms.py
@@ -3,14 +3,19 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, EmailField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, Regexp
 
+from config.default import ALLOWED_EXTENSIONS
+
+
+allowed = list(ALLOWED_EXTENSIONS)
+
 
 class SignUpForm(FlaskForm):
     username = StringField('아이디', validators=[DataRequired('아이디는 필수입력 항목입니다.'), Length(min=3, max=25, message='아이디는 3~25자 이내여야 합니다'), Regexp('^[A-Za-z][A-Za-z0-9]*$', message='아아디는 영문 및 숫자만 사용할 수 있습니다.')])
     nickname = StringField('닉네임', validators=[DataRequired('닉네임은 필수입력 항목입니다.'), Length(min=2, max=25, message='닉네임은 2~25자 이내여야 합니다')])
     password = PasswordField('비밀번호', validators=[DataRequired('비밀번호는 필수입력 항목입니다.'), EqualTo('password_validate', '비밀번호가 일치하지 않습니다')])
     password_validate = PasswordField('비밀번호 확인', validators=[DataRequired('비밀번호 확인은 필수입력 항목입니다.')])
-    email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email()])
-    profile_image = FileField('프로필 사진', validators=[FileAllowed(['png', 'jpg', 'jpeg', 'gif', 'webp'], '프로필 사진은 이미지 파일만 업로드할 수 있습니다.')])
+    email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email('이메일 형식이 올바르지 않습니다.')])
+    profile_image = FileField('프로필 사진', validators=[FileAllowed(allowed, '프로필 사진은 이미지 파일만 업로드할 수 있습니다.')])
     about_me = StringField('자기소개')
 
 
@@ -24,8 +29,8 @@ class ProfileModifyForm(FlaskForm):
     nickname = StringField('닉네임', validators=[DataRequired('닉네임은 필수입력 항목입니다.'), Length(min=2, max=25, message='닉네임은 2~25자 이내여야 합니다')])
     password = PasswordField('비밀번호', validators=[EqualTo('password_validate', '비밀번호가 일치하지 않습니다')])
     password_validate = PasswordField('비밀번호 확인')
-    email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email()])
-    profile_image = FileField('프로필 사진', validators=[FileAllowed(['png', 'jpg', 'jpeg', 'gif', 'webp'], '프로필 사진은 이미지 파일만 업로드할 수 있습니다.')])
+    email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email('이메일 형식이 올바르지 않습니다.')])
+    profile_image = FileField('프로필 사진', validators=[FileAllowed(allowed, '프로필 사진은 이미지 파일만 업로드할 수 있습니다.')])
     about_me = StringField('자기소개')
 
 
@@ -51,9 +56,9 @@ class MageForm(FlaskForm):
     activation_time = StringField('균열 마법사 능력 발동 시기', validators=[DataRequired('균열 마법사 능력 발동 시기는 필수입력 항목입니다.')])
     activation_time_en = StringField('균열 마법사 능력 발동 시기 영어', validators=[DataRequired('균열 마법사 능력 발동 시기 영어는 필수입력 항목입니다.')])
     required_charges = StringField('균열 마법사 능력 요구 충전수', validators=[DataRequired('균열 마법사 능력 요구 충전수는 필수입력 항목입니다.'), Regexp('^[0-9]*$', message='균열 마법사 능력 요구 충전수는 숫자만 입력할 수 습니다.')])
-    image = FileField('균열 마법사 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '균열 마법사 이미지는 이미지 파일만 업로드할 수 있습니다.')])
-    board_image = FileField('균열 마법사 보드 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '균열 마법사 보드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
-    back_board_image = FileField('균열 마법사 보드 뒷면 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '균열 마법사 보드 뒷면 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    image = FileField('균열 마법사 이미지', validators=[FileAllowed(allowed, '균열 마법사 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    board_image = FileField('균열 마법사 보드 이미지', validators=[FileAllowed(allowed, '균열 마법사 보드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    back_board_image = FileField('균열 마법사 보드 뒷면 이미지', validators=[FileAllowed(allowed, '균열 마법사 보드 뒷면 이미지는 이미지 파일만 업로드할 수 있습니다.')])
 
 
 class CardForm(FlaskForm):
@@ -66,7 +71,7 @@ class CardForm(FlaskForm):
     effect_en = TextAreaField('카드 영어 효과', validators=[DataRequired('카드 영어 효과는 필수입력 항목입니다.')])
     tier = StringField('카드 티어', validators=[DataRequired('카드 티어는 필수입력 항목입니다.'), Regexp('^[0-9]*$', message='카드 티어는 숫자만 입력할 수 있습니다.')])
     hp = StringField('카드 체력', validators=[DataRequired('카드 체력은 필수입력 항목입니다.'), Regexp('^[0-9]*$', message='카드 체력은 숫자만 입력할 수 있습니다.')])
-    image = FileField('카드 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '카드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    image = FileField('카드 이미지', validators=[FileAllowed(allowed, '카드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
 
 
 class NemesisForm(FlaskForm):
@@ -85,9 +90,9 @@ class NemesisForm(FlaskForm):
     unleash_en = TextAreaField('Unleash (English)', validators=[DataRequired('Unleash (English)는 필수입력 항목입니다.')])
     increased_diff = TextAreaField('어려운 난이도 규칙', validators=[DataRequired('어려운 난이도 규칙은 필수입력 항목입니다.')])
     increased_diff_en = TextAreaField('어려운 난이도 규칙 (English)', validators=[DataRequired('어려운 난이도 규칙 (English)은 필수입력 항목입니다.')])
-    image = FileField('네메시스 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '네메시스 이미지는 이미지 파일만 업로드할 수 있습니다.')])
-    board_image = FileField('네메시스 보드 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '네메시스 보드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
-    back_board_image = FileField('네메시스 보드 뒷면 이미지', validators=[FileAllowed(['png', 'jpg', 'jpeg'], '네메시스 보드 뒷면 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    image = FileField('네메시스 이미지', validators=[FileAllowed(allowed, '네메시스 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    board_image = FileField('네메시스 보드 이미지', validators=[FileAllowed(allowed, '네메시스 보드 이미지는 이미지 파일만 업로드할 수 있습니다.')])
+    back_board_image = FileField('네메시스 보드 뒷면 이미지', validators=[FileAllowed(allowed, '네메시스 보드 뒷면 이미지는 이미지 파일만 업로드할 수 있습니다.')])
 
 
 class ReviewForm(FlaskForm):
